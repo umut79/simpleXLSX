8f212482c07e64b87756c5fbbdaac364fdb71f47
UmutA <umut.acikgoz@gmail.com>
2024-02-25 17:11:01 +0300

    Update backup_db.php

--- simplexlsx/backup_db.php
+++ simplexlsx/backup_db.php
@@ -1,8 +1,25 @@
 <?php
-
+// çalışmıyor
 $filename='database_backup_'.date('G_a_m_d_y').'.sql';
 
-$result=exec('mysqldump simple_xlsx --password=4200 --user=root --single-transaction >/backups/'.$filename,$output);
+
+//$result=exec('mysqldump simple_xlsx --password=4200 --user=root --single-transaction >/backups/'.$filename,$output);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$database = 'simple_xlsx';
+$user = 'root';
+$pass = '4200';
+$host = 'localhost';
+$dir = dirname(__FILE__) . '/'. $filename;
+
+echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
+
+exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
+
+var_dump($output);
 
 if(empty($output)){
 /* no output is good */
